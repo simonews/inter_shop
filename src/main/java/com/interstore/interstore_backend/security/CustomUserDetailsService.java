2da6ba9e5060c168efa43a310d148d7debd1f7e6
simonews <simonegoffredows@gmail.com>
2025-08-17 16:48:40 +0200

    implementazione logica di sicurezza con Spring Security & Jwt

---
+++ src/main/java/com/interstore/interstore_backend/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.interstore.interstore_backend.security;
+
+import com.interstore.interstore_backend.entity.User;
+import com.interstore.interstore_backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
+
---
+++ src/main/java/com/interstore/interstore_backend/service/UserService.java
@@ -0,0 +1,23 @@
+package com.interstore.interstore_backend.service;
+
+import com.interstore.interstore_backend.entity.User;
+import com.interstore.interstore_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
